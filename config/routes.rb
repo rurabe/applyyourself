a33674059cda14f872859f8bda4e9760574befe4
Ryan Urabe <rurabe@gmail.com>
2012-04-28 03:32:00 -0700

    trying again

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Schoolapplier::Application.routes.draw do
   resources :schools
   
-  root :to => 'schools_path'
+  root :to => schools_path
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
