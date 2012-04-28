7e0f3865a9a9a90eef080f59634e458a65c5db37
Ryan Urabe <rurabe@gmail.com>
2012-04-28 02:20:20 -0700

    changed form styling and routed to schools#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Schoolapplier::Application.routes.draw do
   resources :schools
+  
+  root :to => 'schools#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
