eb150698e22be3f2d4a48b1790015ee48fbe8289
Ryan Urabe <rurabe@gmail.com>
2012-06-06 20:21:12 -0700

    added users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120429052148) do
+ActiveRecord::Schema.define(:version => 20120607025635) do
 
   create_table "schools", :force => true do |t|
     t.string   "name"
@@ -31,8 +31,11 @@ ActiveRecord::Schema.define(:version => 20120429052148) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
